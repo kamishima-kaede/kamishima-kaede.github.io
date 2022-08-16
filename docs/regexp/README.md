1149d33291120356b7e106dd911a2408a354b602
shiki <kamiacgxu@gmail.com>
2022-08-16 17:54:29 +0800

    add regex

---
+++ docs/regexp/README.md
@@ -0,0 +1,39 @@
+---
+title: 正则表达式学习
+icon: list
+category: 编程
+tag:
+  - regexp
+isOriginal: true
+date: 2022-08-16
+---
+
+
+- [正则表达式字符匹配](matching.md)
+- [正则表达式位置匹配]()
+- [正则表达式括号的作用]()
+- [回溯法原理]()
+- [正则表达式的拆分]()
+- [正则表达式的构建]()
+- [正则表达式的编程]()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+参考资料:
+- [1] [MDN](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/RegExp)
+- [2] [《JavaScript正则表达式迷你书》](https://github.com/qdlaoyao/js-regex-mini-book)
---
+++ docs/regexp/test.js
@@ -0,0 +1,19 @@
+// const regex = /hello/;
+// console.log(regex.test('hello'));
+
+// const reg1 = /ab{2,5}c/g
+// const str = 'abc abbc abbbc abbbbc abbbbbc abbbbbbc'
+// console.log(str.match(reg1));
+// [ 'abbc', 'abbbc', 'abbbbc', 'abbbbbc' ]
+
+
+// const reg2 = /a[^123]b/g;
+// const str2 = "a0b a1b a2b a3b a4b";
+// console.log( str2.match(reg2) );
+// [ 'a1b', 'a2b', 'a3b' ]
+
+
+const reg3 = /\d{2,5}?/g;
+const str3 = "123 1234 12345 123456";
+console.log( str3.match(reg3) );
+// [ '12', '12', '34', '12', '34', '12', '34', '56' ]
