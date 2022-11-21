a9a854225c7e5ca944ffefbb857a29d52137d1c9
kamishima-kaede <39673186+kamishima-kaede@users.noreply.github.com>
2022-11-21 02:40:53 +0000

    Deploying to gh-pages from @ kamishima-kaede/kamishima-kaede.github.io@037178633411e9f8aaffdccdb48eea2441a82243 🚀

---
+++ assets/fileCcommand.html.4dac5fda.js
@@ -0,0 +1,14 @@
+import{_ as n}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as s,c as a,d as e}from"./app.641adfb4.js";const l={},i=e(`<div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token builtin class-name">pwd</span> <span class="token comment"># print working directory（打印工作目录）</span>
+
+<span class="token function">ls</span> <span class="token comment"># 当前位置文件和目录 list 的简写</span>
+
+<span class="token function">ls</span> <span class="token parameter variable">-a</span> <span class="token comment"># 命令的参数-a 是--all 的简写，意思是列出所有文件，包括不加任何参数的ls 能够列出的文件和以. 开头的文件，即隐藏（hidden）文件</span>
+
+<span class="token builtin class-name">cd</span> <span class="token comment"># change directory 切换目录</span>
+
+<span class="token function">ls</span> <span class="token parameter variable">-l</span> <span class="token comment">#参数-l，意思是输出文件的详细内容（long listing format）</span>
+<span class="token function">ls</span> <span class="token parameter variable">-l</span> /etc/sudoers <span class="token comment">#可查看该文件的所有者</span>
+<span class="token function">groups</span> root achao
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),c=[i];function t(o,d){return s(),a("div",null,c)}const p=n(l,[["render",t],["__file","fileCcommand.html.vue"]]);export{p as default};
---
+++ assets/pageview.c8a519e9.js
@@ -0,0 +1,1 @@
+import{i as e}from"./app.641adfb4.js";const a=()=>e({serverURL:"https://blog-q5gt9jo8g-kamishima-kaede.vercel.app/"});export{a as updatePageview};
