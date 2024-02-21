06e877d22bb1548bc1fef03fcc14e4850c202ae4
kamishima-kaede <39673186+kamishima-kaede@users.noreply.github.com>
2024-02-21 09:15:08 +0000

    Deploying to gh-pages from @ kamishima-kaede/kamishima-kaede.github.io@c0db93615f083fdaaabc302f4d794156168f8945 🚀

--- assets/pageview-DDEhB-xF.js
+++
@@ -1,7 +0,0 @@
-import{_ as r}from"./app-BmsF6tE2.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://blog-q5gt9jo8g-kamishima-kaede.vercel.app/"};const o=async()=>{try{const{pageviewCount:e}=await r(()=>import("./app-BmsF6tE2.js").then(t=>t.X),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!")}};export{o as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
